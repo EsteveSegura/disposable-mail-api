b5f172ae1e3bbd7dfd3e3595d22c134ecfea2303
Esteve Segura <estevesegura.gir@gmail.com>
2023-02-04 21:54:27 +0100

    show html response

--- src/responses/InboxResponse.js
+++ src/responses/InboxResponse.js
@@ -5,7 +5,7 @@ class InboxResponse {
       from: mail.from,
       subject: mail.subject,
       intro: mail.text.length > 25 ? mail.text.substring(0, (25 - 3)) + '...' : mail.text,
-      text: mail.text,
+      text: mail.html,
     }));
   }
 }
